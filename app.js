14226b45011121e393f8dbec9b6dcab3be4a791b
Ranim-Rabah <79060004+Farah-Torkhani@users.noreply.github.com>
2023-08-19 12:26:05 +0100

    starting the post/get method

---
+++ app.js
@@ -0,0 +1,36 @@
+const express =  require ('express');
+const app = express();
+const bodyParser =  require ('body-parser');
+
+//midleware
+app.use(bodyParser.json());
+
+
+
+require ('dotenv/config');
+
+
+const api = process.env.API_URL;
+
+
+
+app.get(`${api}/products`,(req,res)=>{
+const product = {
+    id: 1,
+    name: 'hair dresser',
+    image: 'some_url',
+}
+    res.send(product) ;
+})
+
+
+app.post(`${api}/products`,(req,res)=>{
+const newProduct = req.body;
+console.log(newProduct);
+    res.send(newProduct) ;
+})
+
+app.listen(3000, ()=>{
+
+    console.log('server is running http://localhost:3000')
+})
