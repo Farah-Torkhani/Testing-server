4f0d8fc6771cd67f70475e4b2f534d57eab0e906
Ranim-Rabah <79060004+Farah-Torkhani@users.noreply.github.com>
2023-08-19 13:04:56 +0100

    posting a product in eshop

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express =  require ('express');
 const app = express();
 const bodyParser =  require ('body-parser');
-
+const morgan = require('morgan');
+require ('dotenv/config');
+const mongoose = require ('mongoose');
 //midleware
 app.use(bodyParser.json());
+app.use(morgan('tiny'));
 
+const productSchema = mongoose.Schema({
 
+    name: String,
+    image: String,
+    countInStock: Number
+})
 
-require ('dotenv/config');
-
+const Product = mongoose.model('Product', productSchema);
 
 const api = process.env.API_URL;
 
@@ -23,11 +30,41 @@ const product = {
     res.send(product) ;
 })
 
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'eshop-database'
+})
+.then(()=>{
+    console.log('Database connection is ready')
+})
+.catch((err)=>{
+    console.log(err);
+})
 
 app.post(`${api}/products`,(req,res)=>{
-const newProduct = req.body;
-console.log(newProduct);
-    res.send(newProduct) ;
+const product = new Product({
+
+    name: req.body.name,
+    image: req.body.image,
+    countInStock: req.body.countInStock,
+    
+})
+
+
+product.save()
+    .then(createdProduct => {
+        res.status(201).json(createdProduct);
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err,
+            success: false
+        });
+    });
+
+console.log(product);
+    res.send(product) ;
 })
 
 app.listen(3000, ()=>{
