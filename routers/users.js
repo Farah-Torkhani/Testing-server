d57b53567726b576f2e13f67fa82be88b01d08db
Ranim-Rabah <79060004+Farah-Torkhani@users.noreply.github.com>
2023-08-28 15:22:01 +0100

    adding users

--- routers/users.js
+++ routers/users.js
@@ -11,4 +11,30 @@ router.get(`/`, async (req, res) =>{
     res.send(userList);
 })
 
+
+router.post('/',async(req,res)=>{  
+        let user = new User({
+
+            name: req.body.name,
+            email: req.body.email,
+            color:req.body.color,
+            passwordHash: req.body.passwordHash,
+            phone:req.body.phone,
+            isAdmin: req.body.isAdmin,
+            appartment: req.body.appartment,
+            zip: req.body.zip,
+           city: req.body.city,
+           country: req.body.country,
+
+
+
+
+        })
+    user = await user.save();
+    if(!user){
+    return res.status(404).send('the user cannot be created!')
+    }
+        res.send(user);
+})
+
 module.exports =router;
