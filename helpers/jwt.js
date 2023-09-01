df789f0545dd1442fdf0d4f6e7fa458b7608bab1
Ranim-Rabah <79060004+Farah-Torkhani@users.noreply.github.com>
2023-09-01 21:59:33 +0100

    authentification and token done

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,11 +1,31 @@
-const expressJwt = require('express-jwt');
+const { expressjwt: ejwt } = require("express-jwt");
 
-function authJwt(){
-const secret = process.env.secret;
-return expressJwt({
-    secret,algorithms: ['HS256']
-})
+function authJwt() {
+    const secret = process.env.secret;
+    const api = process.env.API_URL;
 
+    const expressJwtMiddleware = ejwt({
+        secret: secret, // Use your actual secret here
+        algorithms: ['HS256'],
+        isRevoked: isRevoked
+    }).unless({
+        path: [
+            {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
+            {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
+            `${api}/users/login`,
+            `${api}/users/register`,
+        ]
+    });
+
+    return expressJwtMiddleware;
+}
+
+async function isRevoked(req, payload, done) {
+    if (!payload.isAdmin) {
+        done(null, true);
+    } else {
+        done();
+    }
 }
 
 module.exports = authJwt;
